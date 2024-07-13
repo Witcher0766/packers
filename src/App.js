7b738b01ae84a3f27b56e3113fa924bcaae16699
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-07-13 16:56:41 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-  <div>helljgjkkjhkjkhkhjhjkho</div>
+  <main className='container'>
+    <Header/>
+
+    <Footer/>
+  </main>
   );
 }
 
