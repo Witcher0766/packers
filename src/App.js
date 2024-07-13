6ba421a516092fd80d079f121011741efb33e34d
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-07-13 17:22:54 +0530

    conflict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-<<<<<<< Updated upstream
 import Footer from './components/Footer';
 import Header from './components/Header';
-
-function App() {
-  return (
-  <main className='container'>
-    <Header/>
-
-    <Footer/>
-  </main>
-=======
 import { Button } from "flowbite-react";
 
 
 function App() {
   return (
-   <div>helljgjkkjhkjkhkhjhjkho</div>
->>>>>>> Stashed changes
+    <main className='main-container'>
+   <div className='container'>
+   <Header/>
+    <Footer/>
+   </div>
+  </main>
   );
 }
 
